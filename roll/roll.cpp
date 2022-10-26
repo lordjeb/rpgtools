98ac726a672c4600e9fe8b8ec75e32e00a0c048b
Jeremy Hurren <lordjeb@lordjeb.com>
2022-10-25 22:25:17 -0600

    Handle advantage and keeping best n rolls

--- roll/roll.cpp
+++ roll/roll.cpp
@@ -27,32 +27,33 @@ class expression_evaluator
 {
     random_number_generator* rng_;
 
-    enum class operator_assocation { none, left_to_right, right_to_left };
+    enum class assocativity { left_to_right, right_to_left };
 
     struct operator_info
     {
         int precedence;
-        operator_assocation associativity;
+        assocativity associativity;
     };
 
     std::unordered_map<std::string, operator_info> operators = {
         // clang-format off
-        { "+", { 2, operator_assocation::left_to_right } },
-        { "-", { 2, operator_assocation::left_to_right } },
+        { "+", { 2, assocativity::left_to_right } },
+        { "-", { 2, assocativity::left_to_right } },
         // clang-format on
     };
 
-    int precedence(const std::string& op)
+    int get_precedence(const std::string& op)
     {
         return operators[op].precedence;
     }
-    operator_assocation associativity(const std::string& op)
+    assocativity get_associativity(const std::string& op)
     {
         return operators[op].associativity;
     }
 
 public:
     enum class token_type { number, operation, left_parenthesis, right_parenthesis, dice_expression };
+    enum class keeping_mode { all, best, worst };
 
     expression_evaluator(random_number_generator* rng) : rng_{ rng }
     {
@@ -112,7 +113,7 @@ public:
 
     auto evaluate_dice_expression(const std::string& token, std::vector<std::string>& rolls) -> int
     {
-        const std::regex expr{ "(\\d*)[dD](\\d+)" };
+        const std::regex expr{ "(\\d*)[dD](\\d+)([bBwW]?)(\\d*)" };
         std::smatch match;
         if (!std::regex_match(token, match, expr))
         {
@@ -121,19 +122,55 @@ public:
 
         auto num_rolls = match[1].str().empty() ? 1 : std::stoi(match[1].str());
         auto dice_size = std::stoi(match[2].str());
+        auto keeping_mode = get_keeping_mode(match[3].str());
+        auto keeping_count = match[4].str().empty() ? 0 : std::stoi(match[4].str());
 
+        // Roll all the dice
         std::vector<int> dice_rolls;
         for (auto i = 0; i < num_rolls; ++i)
         {
             dice_rolls.emplace_back(rng_->generate(1, dice_size));
         }
 
-        auto result = std::accumulate(dice_rolls.begin(), dice_rolls.end(), 0);
+        // Figure out which dice to keep and sum
+        int result{};
+        std::vector<int> dropped_dice_rolls;
+        switch (keeping_mode)
+        {
+        case keeping_mode::all:
+            break;
+
+        case keeping_mode::best:
+            while (dice_rolls.size() > keeping_count)
+            {
+                auto smallest = std::min_element(dice_rolls.begin(), dice_rolls.end());
+                dropped_dice_rolls.push_back(*smallest);
+                dice_rolls.erase(smallest);
+            }
+            break;
+
+        case keeping_mode::worst:
+            while (dice_rolls.size() > keeping_count)
+            {
+                auto largest = std::max_element(dice_rolls.begin(), dice_rolls.end());
+                dropped_dice_rolls.push_back(*largest);
+                dice_rolls.erase(largest);
+            }
+            break;
+
+        default:
+            throw std::runtime_error("Invalid dice modifier: " + match[3].str());
+        }
+
+        // Sum up the dice rolls that were not dropped
+        result = std::accumulate(dice_rolls.begin(), dice_rolls.end(), 0);
 
         std::stringstream roll_description_stream;
         roll_description_stream << '(';
         std::copy(dice_rolls.begin(), dice_rolls.end(), std::ostream_iterator<int>(roll_description_stream, ", "));
-        
+        std::copy(dropped_dice_rolls.begin(), dropped_dice_rolls.end(),
+                  std::ostream_iterator<int>(roll_description_stream, ", "));
+
         auto roll_description = roll_description_stream.str();
         roll_description.pop_back();
         roll_description.pop_back();
@@ -166,7 +203,7 @@ public:
         }
     }
 
-    auto get_token_type(const std::string token) -> token_type
+    auto get_token_type(const std::string& token) -> token_type
     {
         if (!token.empty() &&
             std::find_if(token.begin(), token.end(), [](char c) { return !std::isdigit(c); }) == token.end())
@@ -191,6 +228,26 @@ public:
         }
 
         throw std::runtime_error("Unexpected token type: " + token);
+    }
+
+    auto get_keeping_mode(const std::string& m) -> keeping_mode
+    {
+        if (m.empty())
+        {
+            return keeping_mode::all;
+        }
+
+        switch (std::tolower(m[0]))
+        {
+        case 'b':
+            return keeping_mode::best;
+
+        case 'w':
+            return keeping_mode::worst;
+
+        default:
+            throw std::runtime_error("Invalid dice modifier: " + m);
+        }
     }
 
     auto parse(const std::string expression) -> std::vector<std::string>
@@ -224,34 +281,49 @@ public:
                 break;
 
             case token_type::left_parenthesis:
-            case token_type::right_parenthesis:
+                stack.push(token);
+                break;
+
+            case token_type::right_parenthesis: {
+                // Pop from stack and push to result until you get a left paren
+                std::string top_token{};
+                while (!stack.empty())
+                {
+                    top_token = stack.top();
+                    if (get_token_type(top_token) != token_type::left_parenthesis)
+                    {
+                        result.emplace_back(top_token);
+                    }
+                    stack.pop();
+                }
+
+                if (get_token_type(top_token) != token_type::left_parenthesis)
+                {
+                    throw std::runtime_error("No matching parenthesis");
+                }
+            }
+            break;
+
             case token_type::operation:
-                if (token_type != token_type::left_parenthesis)
+                while (!stack.empty())
                 {
-                    // clang-format off
-                    while (!stack.empty() &&
-                           (
-                               (token_type == token_type::right_parenthesis && get_token_type(stack.top()) != token_type::left_parenthesis) ||
-                               precedence(stack.top()) > precedence(token) ||
-                               (precedence(stack.top()) == precedence(token) && associativity(token) == operator_assocation::left_to_right)
-                           )
-                        )
-                    // clang-format on
+                    auto top_token = stack.top();
+                    auto top_token_precedence = get_precedence(top_token);
+                    auto token_precedence = get_precedence(token);
+
+                    if (top_token_precedence > token_precedence ||
+                        (top_token_precedence == token_precedence &&
+                         get_associativity(token) == assocativity::left_to_right))
                     {
-                        result.emplace_back(stack.top());
+                        result.emplace_back(top_token);
                         stack.pop();
                     }
-
-                    if (token_type == token_type::right_parenthesis)
+                    else
                     {
-                        stack.pop();
+                        break;
                     }
                 }
-
-                if (token_type != token_type::right_parenthesis)
-                {
-                    stack.push(token);
-                }
+                stack.push(token);
                 break;
 
             default:
@@ -346,7 +418,7 @@ TEST_F(expression_evaluator_test, parse_3)
 
 TEST_F(expression_evaluator_test, convert_infix_to_prefix_1)
 {
-    auto result = eval.convert_infix_to_prefix(std::vector<std::string>{ "1", "+", "1" });
+     auto result = eval.convert_infix_to_prefix(std::vector<std::string>{ "1", "+", "1" });
     EXPECT_THAT(result, ContainerEq(std::vector<std::string>{ "1", "1", "+" }));
 }
 
@@ -413,4 +485,35 @@ TEST_F(expression_evaluator_test, multiple_d6_roll)
     auto result = eval.evaluate("2d6", &description);
     EXPECT_THAT(result, Eq(7));
     EXPECT_THAT(description, StrEq("(4, 3)"));
+}
+
+TEST_F(expression_evaluator_test, roll_with_advantage)
+{
+    EXPECT_CALL(rng, generate(1, 20)).Times(2).WillOnce(Return(3)).WillOnce(Return(17));
+    auto result = eval.evaluate("2d20b1", &description);
+    EXPECT_THAT(result, Eq(17));
+    EXPECT_THAT(description, StrEq("(17, 3)")); // Note that this will always print the dropped dice after the good ones
+}
+
+TEST_F(expression_evaluator_test, roll_with_disadvantage)
+{
+    EXPECT_CALL(rng, generate(1, 20)).Times(2).WillOnce(Return(3)).WillOnce(Return(17));
+    auto result = eval.evaluate("2d20w1", &description);
+    EXPECT_THAT(result, Eq(3));
+    EXPECT_THAT(description,
+                StrEq("(3, 17)"));   // Note that this will always print the dropped dice after the good ones
+}
+
+TEST_F(expression_evaluator_test, roll_4d6_keep_best_3)
+{
+    EXPECT_CALL(rng, generate(1, 6))
+        .Times(4)
+        .WillOnce(Return(3))
+        .WillOnce(Return(3))
+        .WillOnce(Return(5))
+        .WillOnce(Return(6));
+    auto result = eval.evaluate("4d6b3", &description);
+    EXPECT_THAT(result, Eq(14));
+    EXPECT_THAT(description,
+                StrEq("(3, 5, 6, 3)"));   // Note that this will always print the dropped dice after the good ones
 }
